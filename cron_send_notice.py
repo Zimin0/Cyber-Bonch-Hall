b3fd8b9453c0ba9121e15bf3fc8ae4b5337fd31e
Zimin0 <you@example.com>
2023-06-15 22:47:30 +0300

    Temp

--- cron_send_notice.py
+++ cron_send_notice.py
@@ -8,23 +8,24 @@ def send_notification():
     from notifications.models import Notification
     from booking.models import TimePeriod
 
-    try:
-        notifs = Notification.objects.exclude(status="S")
-        for notif in notifs:
-            if notif.status in ('W', 'NTS') : # Уведомление ожидает отправки
-                time_now = f"{notif.time.hour}:{notif.time.second}"
-                if TimePeriod.start_end_time_to_sec(time_now) <= TimePeriod.get_now_in_sec(): # если пришло время уведомления
-                    send_message(notif.user_vk_id, notif.text)
-                    notif.status = 'S'
-                    notif.save()
-    except Exception as e:
-        print(e)
+    notifs = Notification.objects.exclude(status="S")
+    
+    print(f"Нашел {notifs.count()} неотправленных уведомдений.")
+    for notif in notifs:
+        if notif.status in ('W', 'NTS') : # Уведомление ожидает отправки
+            time_now = f"{notif.time.hour}:{notif.time.second}"
+            if TimePeriod.start_end_time_to_sec(time_now) <= TimePeriod.get_now_in_sec(): # если пришло время уведомления
+                send_message(notif.user_vk_id, notif.text)
+                print(f"Успешно оправлено уведомление №{notif.pk}")
+                notif.status = 'S'
+                notif.save()
+
         ## Добавить логирование
 
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
-    django.setup()
-    send_notification()
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+django.setup()
+send_notification()
 
 
 """
