112dd98923969e620d93ecfd430586be7493b08b
Zimin0 <you@example.com>
2023-06-15 23:28:47 +0300

    Temp

--- cron_send_notice.py
+++ cron_send_notice.py
@@ -12,7 +12,7 @@ def send_notification():
 
     print(f"Нашел {notifs.count()} неотправленных уведомдений.")
     for notif in notifs:
-        print(f"Уведомление №{notif.pk}")
+        print(f"Уведомление №{notif.pk}. Его статус={notif.status}")
         if notif.status in ('W', 'NTS') : # Уведомление ожидает отправки
             if TimePeriod.start_end_time_to_sec(notif.time) <= TimePeriod.get_now_in_sec(): # если пришло время уведомления
                 send_message(notif.user_vk_id, notif.text)
